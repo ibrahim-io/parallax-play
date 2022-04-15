66243beb42b8fa793b96c0278437301378c9931e
Ibrahim Ibrahim <ii83326@gmail.com>
2022-04-15 14:59:34 +0100

    feat: playing around with parallax

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import {Parallax, ParallaxLayer} from '@react-spring/parallax'
+import moon from './red-moon.jpg'
+import jet from './night-jet.jpg'
+import city from './night-city.jpg'
+import parcel from './output-onlinegiftools.gif'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <Parallax pages={3}>
+        <ParallaxLayer 
+          offset={0}
+          speed={1}
+          factor={1}
+          style= {{
+            backgroundImage: `url(${moon})`,
+            backgroundSize: 'cover'
+          }}
+          >
+          <h2>Welcome to my website</h2>
+        </ParallaxLayer>
+
+        <ParallaxLayer 
+          offset={1} 
+          speed={1.2}
+          factor={1}
+          style={{
+            backgroundImage: `url(${jet})`,
+            backgroundSize: 'contain',
+            backgroundPosition: 'center'
+          }}>
+        </ParallaxLayer>
+
+        <ParallaxLayer 
+          offset={2} 
+          speed={0.5}
+          factor={1}
+          style={{
+            backgroundImage: `url(${city})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center'
+          }}>
+          <h2>Web Dev is fun</h2>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          sticky={{start: 0.5, end: 2.5}}
         >
-          Learn React
-        </a>
-      </header>
+          <img src={parcel} alt='floating parcel'/>
+        </ParallaxLayer>
+      </Parallax>
+
     </div>
   );
 }
